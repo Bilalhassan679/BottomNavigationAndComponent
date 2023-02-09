8992a137c839c6aef660867d44c18915073b3841
Mbilal6799 <ftk4321@gmail.com>
2023-02-09 09:52:17 -0800

    Radio Button

---
+++ src/Components/RadioButton/RadioButtion.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React,{ useState } from 'react'
+
+const RadioButtion = () => {
+    const [bool, setBool] = useState(false);
+  return (
+    <View>
+      <Text>RadioButtion</Text>
+    </View>
+  )
+}
+
+export default RadioButtion
+
+// const styles = StyleSheet.create({})
